98a8e0862f1116f5562440f0bc1da98bad92e17d
chrisalopez1337 <chrisalopez@outlook.com>
2021-04-16 21:46:01 -0700

    Added basic frontend for testing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+    const [shortLink, setShortLink] = useState(null);
+    const [redirect_link, setRedirectLink] = useState('');
+    function handleChange(e) {
+        setRedirectLink(e.target.value);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios.post('/api/links/create', { redirect_link })
+            .then(({ data }) => {
+                const { short_link } = data;
+                // have to make a dummy link for testing
+                let hash = short_link.split('/');
+                hash = hash[1];
+                const testLink = `localhost:1337/${hash}`;
+                setShortLink(testLink);
+            })
+            .catch(console.log);
+    }
     return (
         <>
-            <h1>Test</h1>
+            <h1>Get Link</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="redirect_link">Link to shorten</label>
+                <input type="text" name="redirect_link" value={redirect_link} onChange={handleChange}/>
+                <button type="submit">get short link</button>
+            </form>
+            {shortLink ? <h1>{shortLink}</h1> : null }
         </>
     );
 };
