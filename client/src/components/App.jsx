15c393480545ebbae0c2035f6a7a24d7b2c83f72
chrisalopez1337 <chrisalopez@outlook.com>
2021-04-17 01:33:42 -0700

    Added basic styling for the link upon search

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
     return (
         <Container>
             <Header />
-            <Converter />
+            <Converter shortLink={shortLink} handleShortLink={handleChange} handleSubmit={handleSubmit} redirect_link={redirect_link}/>
         </Container>
     );
 };
--- client/src/components/Converter.jsx
+++ client/src/components/Converter.jsx
@@ -9,7 +9,7 @@ const Container = styled.form`
     justify-content: center;
     flex-direction: column;
     border: 2px solid #7830f2;
-    padding: 20px 50px 60px 50px;
+    padding: 20px 50px 40px 50px;
     border-radius: 7px;
     box-shadow: 0px 0px 15px 0px #7830f2;
 `;
@@ -25,8 +25,10 @@ const Input = styled.input`
    border-radius: 10px;
    padding: 5px;
    font-family: inherit;
+   font-weight: 400;
    font-size: 17px;
    width: 350px;
+   margin-bottom: 20px;
 `;
 
 const Row = styled.div`
@@ -47,20 +49,30 @@ const SubmitButton = styled.button`
    margin-left: 5px;
    cursor: pointer;
    transition-duration: 0.2s;
+   margin-bottom: 20px;
    &:hover {
         background-color: #7830f2;
         box-shadow: 0px 0px 15px 0px #7830f2;
    }
 `;
 
-export default function Converter() {
+const Link = styled.h2`
+    padding: 7px;
+    background-color: #c9bfc3;
+    color: #af36f5;
+    font-size: 20px;
+    border-radius: 7px;
+`;
+
+export default function Converter({ shortLink, handleShortLink, handleSubmit, redirect_link }) {
     return (
-        <Container>
+        <Container onSubmit={handleSubmit}>
             <Title>Shorten URL</Title>
             <Row>
-                <Input type="text" name="redirect_url" />
+                <Input type="text" name="redirect_url" value={redirect_link} onChange={handleShortLink} />
                 <SubmitButton>Shorten</SubmitButton>
             </Row>
+            {shortLink?.length > 0 ? <Link>{shortLink}</Link> : null }
         </Container>
     );
 }
