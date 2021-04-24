66d7057d18ffc3b44a7230d8672c1a51cc9c4f57
chrisalopez1337 <chrisalopez@outlook.com>
2021-04-23 22:29:19 -0700

    Added HTTPS server

--- server/index.js
+++ server/index.js
@@ -6,21 +6,29 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const linkRouter = require('./linkRoutes.js');
 const userRouter = require('./userRoutes.js');
+const fs = require('fs');
+const https = require('https');
+
+// HTTPS
+const key = fs.readFileSync(__dirname + '../selfsigned.key');
+const cert = fs.readFileSync(__dirname + '../selfsigned.crt');
+const options = { key, cert };
+const server = https.createServer(options, app);
 // Redirect controller
 const {sendToRedirectPage} = require('../controllers/linkControllers.js');
-app.use(function(req, res, next) {
+server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
+server.use('/', express.static(path.join(__dirname, '../client/dist')));
 
-app.use('/api/links', linkRouter);
-app.use('/api/users', userRouter);
-app.get('/:hash', sendToRedirectPage);
+server.use('/api/links', linkRouter);
+server.use('/api/users', userRouter);
+server.get('/:hash', sendToRedirectPage);
 
 
-app.listen(PORT, () => console.log(`App running on ${PORT}`));
+server.listen(PORT, () => console.log(`App running on ${PORT}`));
